47a95423ee78e8ef6dea83fc49ca518c8672da39
Zarutian <zarutian@gmail.com>
2016-11-11 23:03:08 +0000

    .
    Fixed glaring errors

--- ocap_examples.lua
+++ ocap_examples.lua
@@ -1,13 +1,26 @@
 -- This file is put here for now.
 
-local packages = []
+local packages = {}
+
+packages["ocap/weakmap"] = function (require)
+  local iface = {}
+  local mt = {}
+  mt["__mode"] = "k"
+  iface["make"] = function ()
+    local ins = {}
+    setmetatable(ins, mt)
+    return ins
+  end
+  return iface
+end
 
 packages["ocap/shallow-read-only"] = function (require)
-  local weakmap = require("ocap/weakmap")
-  local proxy2tab = weakmap.make()
-  local mt = []
+  local proxy2tab = {}
+  local mt = {}
   mt["__newindex"] = function (proxy, idx, val) 
     -- simply ignored
   end
-  mt["__index"] = function (
+  mt["__index"] = function (proxy, idx)
+    return proxy2tab[proxy][idx]
+  end
 end
